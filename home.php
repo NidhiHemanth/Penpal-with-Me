917d0886bb01648fc37c574de4d381b6b87a2ee0
Nidhi <nidhihemanth123@gmail.com>
2021-07-14 20:14:17 +0530

    home page basic

--- home.php
+++ home.php
@@ -10,8 +10,15 @@
 <!DOCTYPE html>
 <head>
     <title>Home</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 <body>
     <h1>Hello world</h1>
+
+    <script src="./script/main.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
