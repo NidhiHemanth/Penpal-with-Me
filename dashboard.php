616f4222ed1875fb9346f516e7e2c83b1df3d687
Nidhi <nidhihemanth123@gmail.com>
2021-07-22 15:50:34 +0530

    until line 165

--- dashboard.php
+++ dashboard.php
@@ -1,23 +1,26 @@
 <?php 
-  $servername = "localhost:3307";
-  $username = "root";
-  $password = "";
-  $dbname = "DBMS";
-   // Create connection
-   $conn = new mysqli($servername, $username, $password, $dbname);
-   // Check connection
-   if ($conn->connect_error) {
-       die("Connection failed: " . $conn->connect_error);
-   }
-             
-    session_start();
 
-    if(!isset($_SESSION['logged_in'])) {
-        header('Location: http://localhost/PHPfiles/PenPals/index.php');
-        exit;
-    }
-   
+	session_start();
+
+	if(!isset($_SESSION['logged_in'])) {
+		header('Location: http://localhost/PHPfiles/PenPals/index.php');
+		exit;
+	}
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "DBMS";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+				
 ?>
+
 <!DOCTYPE html>
 <head>
     <title>PenPals</title>
@@ -28,136 +31,138 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 <body>
-<div class="container-fluid header">
-        <button type="button" class="btn btn-warning setting" data-bs-toggle="modal" data-bs-target="#settingModal"><i class="fa fa-gear fa-spin" style="font-size:24px"></i></button>
+	<div class="container-fluid header">
+        <button type="button" class="btn btn-warning setting" data-bs-toggle="modal" data-bs-target="#settingModal">
+			<i class="fa fa-gear fa-spin" style="font-size:24px"></i>
+		</button>
         <div class="modal fade" id="settingModal" tabindex="-1" aria-labelledby="settingModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="settingModalLabel">Profile Settings</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="accordion" id="accordionPanelsStayOpenExample">
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                              Your info ;)
-                            </button>
-                          </h2>
-                          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                            <div class="accordion-body">
-                              <strong>Hey, <?php echo $_SESSION['username'].' ';?>.</strong> <br>
-                              We are extremely grateful that you chose to use your website! 
-                              We hope you have an excellent day ^-^
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                              Update Interests
-                            </button>
-                          </h2>
-                          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                            <div class="accordion-body">
-                                <form action="">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Art (Painting / Sketching etc)
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Anime
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Gaming
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Music
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Movies / TV Shows
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Tech field
-                                        </label>
-                                    </div>
-                                    <button type="button" class="btn btn-primary set-profile">Save changes</button>
-                                </form>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                              Update Languages
-                            </button>
-                          </h2>
-                          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                            <div class="accordion-body">
-                                <form action="">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          English
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Hindi
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Spanish
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Chinese
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                          Japanese
-                                        </label>
-                                    </div>
-                                    <button type="button" class="btn btn-primary set-profile">Save changes</button>
-                                </form>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                    <a type="button" class="btn btn-secondary" href="./php/logout.php">Log Out</a>
-                 
-                </div>
-              </div>
-            </div>
-        </div>
-    </div>
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="settingModalLabel">Profile Settings</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+                	<div class="modal-body">
+                    	<div class="accordion" id="accordionPanelsStayOpenExample">
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="panelsStayOpen-headingOne">
+									<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+										Your info ;)
+									</button>
+								</h2>
+								<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+									<div class="accordion-body">
+										<strong>Hey, <?php echo $_SESSION['username'];?>.</strong> <br>
+										We are extremely grateful that you chose to use your website! 
+										We hope you have an excellent day ^-^
+									</div>
+								</div>
+							</div>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+										Update Interests
+									</button>
+								</h2>
+								<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+									<div class="accordion-body">
+										<form action="./php/update_interest.php" method="post">
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Art" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Art (Painting / Sketching etc)
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Anime" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Anime
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Gaming" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Gaming
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Music" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Music
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Binge_Watch" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Movies / TV Shows
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Tech" name="interest[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Tech field
+												</label>
+											</div>
+											<button type="submit" name="submit_interest" class="btn btn-primary set-profile">Save changes</button>
+										</form>
+									</div>
+								</div>
+							</div>
+							<div class="accordion-item">
+								<h2 class="accordion-header" id="panelsStayOpen-headingThree">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+										Update Languages
+									</button>
+								</h2>
+								<div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+									<div class="accordion-body">
+										<form action="./php/update_lang.php" method="post">
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="English" name="lang[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													English
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Hindi" name="lang[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Hindi
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Spanish" name="lang[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Spanish
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Chinese" name="lang[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Chinese
+												</label>
+											</div>
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="Japanese" name="lang[]" id="flexCheckDefault">
+												<label class="form-check-label" for="flexCheckDefault">
+													Japanese
+												</label>
+											</div>
+												<button type="submit" name="submit_lang" class="btn btn-primary set-profile">Save changes</button>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer justify-content-between">
+							<button type="button" class="btn btn-primary">Save changes</button>
+							<a type="button" class="btn btn-secondary" href="./php/logout.php">Log Out</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
     <div class="container first-card">
                   <div class="row d-flex justify-content-center">
@@ -173,7 +178,7 @@
      
        
 
-        <form action = "./php/Addpenpal.php" method="get">
+        <form action = "./php/Addpenpal.php" method="POST">
         <button type="submit" class="btn btn-secondary btn-lg" name="AddPenpal"
                     class="button" value="Get a Penpal!">Get a Penpal!</button>                        
           
@@ -305,9 +310,7 @@
                                                             <div class="mb-3">
                                                             <form method="GET" action="./php/SendMessage.php">
                                                              <textarea name ="Message" class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-                                                             <button type="submit" class="btn btn-outline-secondary" name = "Sends" value = "';
-                                                             echo $_SESSION['pal_'.$i];
-                                                      echo '">Send it!</button>
+                                                             <button type="submit" class="btn btn-outline-secondary" name = "Sends'.$i.'" value = "'.$i.'">Send it!</button>
                                                              </form>
                                                              </div>
                                                           
@@ -372,11 +375,10 @@
                                         <form method="GET" action="./php/delete_pal.php">
                                             <div class="input-group">
                                                 <input type="password" name="confirm_delete" placeholder="Password" class="form-control">
-                                                <button type="submit" class="btn btn-outline-secondary" name = "button';
-                                            echo $i;
-                                                echo '" value = "';
-                                                echo $i;
-                                                echo '">Confirm identity</button> </div>
+                                                <button type="submit" class="btn btn-outline-secondary" name = "button'.$i.'" value = "'.$i.'">
+                                                  Confirm identity
+                                                </button> 
+                                            </div>
                                         </form>
                                     </div>
                                     <div class="modal-footer">
@@ -384,7 +386,6 @@
                                     </div>
     
                                       </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
